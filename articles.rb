c8abc4c5cd6854c3ba400c6343333fef44223157
mwiley <masonwiley92@gmail.com>
2015-01-28 14:46:41 -0800

    Adds variables for the top ad slot.

--- articles.rb
+++ articles.rb
@@ -1,3 +1,5 @@
+@top_ad = {url: '#', image: 'http://placehold.it/450x100'}
+
 @articles = [
     {
         url: 'http://www.ridemagazine.com/horse/article/bit-and-cavasons-cruelly-used-horses-and-horse-named-babe',
@@ -5,23 +7,21 @@
         title: 'Bit And Cavasons Cruelly Used On Horses And A Horse Named Babe',
         preview: 'As you all know, who are my Facebook friends, I was struggling on a topic for my next blog. After your suggestions I was going through my home feed, on Facebook, and I ran across this picture that sparked a need in me to share a story about the cruelty that many horses face in how bits and cavasons are used and how we need to change our attitude and treatment of horses.',
         author: 'Caroyln Resnick',
-        ad_1_url: 'http://www.noworriesclub.com/freedownload/',
-        ad_1_image: 'http://horse-magazine.info/blast/noworriesclub/noworriesclub_newsletter_300x100.jpg',
-        ad_2_url: 'http://oakcroft.com/',
-        ad_2_image: 'http://horse-magazine.info/blast/oakcroft/oakcroft_newsletter_300x100.jpg',
+        ad_1: { url: 'http://www.noworriesclub.com/freedownload/', image: 'http://horse-magazine.info/blast/noworriesclub/noworriesclub_newsletter_300x100.jpg'},
+        ad_2: { url: 'http://oakcroft.com/', image: 'http://horse-magazine.info/blast/oakcroft/oakcroft_newsletter_300x100.jpg'}
     },
     {
         url: 'http://www.ridemagazine.com/horse/article/western-dressage-practice-tips-you-prepare-advance-next-level',
         photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/editorial_main/editorial/6860/lynn-palm-profile-on-paint-.jpg',
         title: 'Western Dressage: Practice Tips As You Prepare To Advance To The Next Level',
         preview: "Now that you've gotten into Western Dressage, you're eager to do well. But what are the best ways to improve your horse at his current level and move on to advance to the next level? As you ride each day, make a plan before you get in the saddle. You are teaching your horse every time you work with him, whether you are taking an actual lesson or not. Be sure he's learning something positive!",
-        author: 'Lynn Palm',
+        author: 'Lynn Palm'
     },
     {
         url: 'http://www.ridemagazine.com/horse/article/getting-results-your-training',
         photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/editorial_main/editorial/6859/charles-wilhelm-2012-headshot-feature.jpg',
         title: 'Getting Results From Your Training',
         preview: 'Someone recently said to me that she had watched me, Richard Winters and some of the other well known horsemen train and had seen the results and the progress a horse could make in one session. She said that when she went home and tried to do the same things, she had difficulties and did not get the same results. She wanted to know what she was doing wrong. I suspect she is not the only one who may have experienced this but I don’t believe it is a matter of doing anything wrong unless there is a lack of understanding in how to do a specific exercise. There is however, a learning curve when working with horses.',
-        author: 'Charles Wilhelm',
-    },
+        author: 'Charles Wilhelm'
+    }
 ]
