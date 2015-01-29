d702051151cd15bf7cb4bfdce4f9c2179954fdd0
mwiley <masonwiley92@gmail.com>
2015-01-29 07:13:50 -0800

    Hides author field when no author exists.

--- articles.rb
+++ articles.rb
@@ -2,26 +2,39 @@
 
 @articles = [
     {
+        title: 'Bit And Cavasons Cruelly Used On Horses And A Horse Named Babe',
         url: 'http://www.ridemagazine.com/horse/article/bit-and-cavasons-cruelly-used-horses-and-horse-named-babe',
         photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/rotator_4-3/editorial/6861/nose-band.png',
-        title: 'Bit And Cavasons Cruelly Used On Horses And A Horse Named Babe',
         preview: 'As you all know, who are my Facebook friends, I was struggling on a topic for my next blog. After your suggestions I was going through my home feed, on Facebook, and I ran across this picture that sparked a need in me to share a story about the cruelty that many horses face in how bits and cavasons are used and how we need to change our attitude and treatment of horses.',
         author: 'Caroyln Resnick',
         ad_1: { url: 'http://www.noworriesclub.com/freedownload/', image: 'http://horse-magazine.info/blast/noworriesclub/noworriesclub_newsletter_300x100.jpg'},
         ad_2: { url: 'http://oakcroft.com/', image: 'http://horse-magazine.info/blast/oakcroft/oakcroft_newsletter_300x100.jpg'}
     },
     {
+        title: 'Western Dressage: Practice Tips As You Prepare To Advance To The Next Level',
         url: 'http://www.ridemagazine.com/horse/article/western-dressage-practice-tips-you-prepare-advance-next-level',
         photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/editorial_main/editorial/6860/lynn-palm-profile-on-paint-.jpg',
-        title: 'Western Dressage: Practice Tips As You Prepare To Advance To The Next Level',
         preview: "Now that you've gotten into Western Dressage, you're eager to do well. But what are the best ways to improve your horse at his current level and move on to advance to the next level? As you ride each day, make a plan before you get in the saddle. You are teaching your horse every time you work with him, whether you are taking an actual lesson or not. Be sure he's learning something positive!",
         author: 'Lynn Palm'
     },
     {
+        title: 'Getting Results From Your Training',
         url: 'http://www.ridemagazine.com/horse/article/getting-results-your-training',
         photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/editorial_main/editorial/6859/charles-wilhelm-2012-headshot-feature.jpg',
-        title: 'Getting Results From Your Training',
-        preview: 'Someone recently said to me that she had watched me, Richard Winters and some of the other well known horsemen train and had seen the results and the progress a horse could make in one session. She said that when she went home and tried to do the same things, she had difficulties and did not get the same results. She wanted to know what she was doing wrong. I suspect she is not the only one who may have experienced this but I don’t believe it is a matter of doing anything wrong unless there is a lack of understanding in how to do a specific exercise. There is however, a learning curve when working with horses.',
+        preview: "Someone recently said to me that she had watched me, Richard Winters and some of the other well known horsemen train and had seen the results and the progress a horse could make in one session. She said that when she went home and tried to do the same things, she had difficulties and did not get the same results. She wanted to know what she was doing wrong. I suspect she is not the only one who may have experienced this but I don't believe it is a matter of doing anything wrong unless there is a lack of understanding in how to do a specific exercise. There is however, a learning curve when working with horses.",
         author: 'Charles Wilhelm'
+    },
+    {
+        title: 'A Horse, Of Course',
+        url: "http://www.ridemagazine.com/horse/article/horse-course-3",
+        photo: 'http://www.ridemagazine.com/sites/default/files/imagecache/editorial_main/editorial/6845/albatross-usta.jpg',
+        preview: "Albatross started life as a small cold out of an unproven mare. No one wanted him. As if that wasn't bad enough, he had that awful name. He wasn't a big, web-footed bird. A group of five men got together to purchase the mare, Voodoo Hanover, and have her bred to Meadow Skipper. The result was Albatross-basically a cull (not a gull). The mare was bred again. This time she had a filly. Nothing much to brag about.",
+        author: 'Don Blazer'
+    },
+    {
+        title: 'World Polo News/Escue Carves Out The Championship Of The 2014 Tackeria Cup',
+        url: 'http://www.ridemagazine.com/horse/news/world-polo-newsescue-carves-out-championship-2014-tackeria-cup',
+        photo: 'https://ci6.googleusercontent.com/proxy/Ku96vR6DpwCDG9B1nv3lx0TgOxisjk09DrAxNPBjentVxiknNBQI-TxnRpsrrzmA1wZjT4UIPo7-EMIcXVoHRkUeZKZ6Vev_hQMlzhdUY3Hnb5ob=s0-d-e1-ft#http://ih.constantcontact.com/fs149/1102176317759/img/3062.png',
+        preview: 'On December 30, spectators lined both sides of the field at International Polo Club Palm Beach in Wellington, Florida. Four teams of young players between the ages of 16-21, battled for the coveted Tackeria Cup. Clean, classic polo charged with the energy that teenagers possess, made for an afternoon of exciting polo.'
     }
 ]
