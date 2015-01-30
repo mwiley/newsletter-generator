63be428290da8a1ea0e697896cecc1dcc6a67dd5
mwiley <masonwiley92@gmail.com>
2015-01-30 10:33:53 -0800

    Adds support for command line arguments for the template and content files

--- make_newsletter.rb
+++ make_newsletter.rb
@@ -1,9 +1,27 @@
+#!/usr/bin/env ruby
 require 'erb'
-load 'articles.rb'
-minify = true
 
+minify = true
+template = ARGV[0].to_s
+content = ARGV[1].to_s
+
+if ARGV.size != 2
+  puts "Invalid number of arguments."
+  exit 1
+else
+  if template.split('.').last != "erb"
+    puts "The first argument should be a template."
+    exit 1
+  end
+  if content.split('.').last != "rb"
+    puts "The second argument should be a .rb file containing content for the template."
+    exit 1
+  end
+end
+
+load content
 File.open("newsletter_out.html", "w") do |file|
-  output = ERB.new(File.read("template/newsletter.html.erb")).result(binding).to_s
+  output = ERB.new(File.read(template)).result(binding).to_s
   output = output.gsub(/(\n|\t|\r)/, ' ').gsub(/>\s*</, '><').squeeze(' ') if minify
   file.write output
 end
