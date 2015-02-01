48b4138c1ce1b4ca3f2c1cf18ac58c9aa5553af0
mwiley <masonwiley92@gmail.com>
2015-01-31 17:33:16 -0800

    Adds template for NDJ sales with no product images.

---
+++ content/ndj_test.rb
@@ -0,0 +1,11 @@
+# The following variables set here will be used to generate a newsletter from the template.
+# @products = [{name: '', url: '' , price_regular: '', price_sale: ''}, ...]
+
+@products = [
+    {
+        name: "5' Schooling standard",
+        url: "https://nextdayjumps.com/horse-jumps/crazy-deals/5-schooling-standard-pair",
+        price_regular: 199,
+        price_sale:  139
+    }
+]
