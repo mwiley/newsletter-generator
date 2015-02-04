8af34da66e214500fc6b97d118b106b7fa2243ed
mwiley <masonwiley92@gmail.com>
2015-02-04 15:01:58 -0800

    Adds README

---
+++ content/ndj_february.rb
@@ -0,0 +1,47 @@
+# @products = [{name: '', url: '' , price_regular: '', price_sale: ''}, ...]
+@template = "template/ndj_noimages.html.erb"
+
+@products = [
+    {
+        name: "5' Schooling standard",
+        url: "https://nextdayjumps.com/horse-jumps/crazy-deals/5-schooling-standard-pair",
+        price_regular: 199,
+        price_sale:  139
+    },
+    {
+        name: "5' Picket Wing Standard",
+        url: 'https://nextdayjumps.com/horse-jumps/standards/5-picket-wing-standards-pair',
+        price_regular: 299,
+        price_sale: 239
+    },
+    {
+        name: "10' x 2' Picket Gate",
+        url: 'https://nextdayjumps.com/horse-jumps/gates/10-x-2-picket-gate',
+        price_regular: 182,
+        price_sale: 139
+    },
+    {
+        name: "10' x 2' Vertical Wall",
+        url: "https://nextdayjumps.com/horse-jumps/walls-blocks/10-x-2-vertical-wall-pvc",
+        price_regular: 229,
+        price_sale: 169
+    },
+    {
+        name: "10' x 2' Brush Box",
+        url: "https://nextdayjumps.com/horse-jumps/brush-boxes/10-x-2-brush-box",
+        price_regular: 229,
+        price_sale: 169
+    },
+    {
+        name: "6' Angle Lattice Wing Standard - Pair",
+        url: "https://nextdayjumps.com/horse-jumps/standards/6-angle-lattice-wing-standard-pair",
+        price_regular: 389,
+        price_sale: 309
+    },
+    {
+        name: "10' x 2' 6\" Triple X Gate",
+        url: "https://nextdayjumps.com/horse-jumps/gates/10-x-2-6-triple-x-gate",
+        price_regular: 219,
+        price_sale: 189
+    }
+]
